84def225ff99734526b72a6fa2bcdb43857b9c09
NSDuman <numanselmanduman@gmail.com>
2022-09-20 19:55:46 +0000

    Create bungalow-detail-view

---
+++ frontend/src/views/bungalow-detail-view.vue
@@ -0,0 +1,30 @@
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'BungalowDetail',
+  components: {},
+  data() {
+    return {
+      bungalow: [],
+    }
+  },
+  async getBungalow() {
+    const bungalowRequest = await axios.get(`http://localhost:4000/api/bungalows/${bungalowId}`)
+
+    this.bungalows = bungalowRequest.data
+  },
+  computed: {
+    bungalowId() {
+      return this.$route.params.id
+    },
+  },
+}
+</script>
+
+<template lang="pug">
+main.bungalow
+  h1 Rent a Bungalow for Your Next Escape
+ 
+
+</template>

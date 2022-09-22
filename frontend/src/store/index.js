4958ed3a8ef68cbef09bae8f63e9dd4d878b34e6
NSDuman <numanselmanduman@gmail.com>
2022-09-22 19:58:06 +0000

    Install and create vuex store

---
+++ frontend/src/store/index.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import { createStore } from 'vuex'
+
+const mutations = {}
+
+export default createStore({
+  state: {},
+  mutations: {},
+  actions: {
+    async fetchBungalow(store, id) {
+      const bungalowRequest = await axios.get(`http://localhost:4000/api/bungalows/${id}`)
+      return bungalowRequest.data
+    },
+    async fetchBungalows() {
+      const bungalowsRequest = await axios.get(`http://localhost:4000/api/bungalows/`)
+      return bungalowsRequest.data
+    },
+    async fetchBookings() {
+      const bookingsRequest = await axios.get(`http://localhost:4000/api/bookings/`)
+      return bookingsRequest.data
+    },
+  },
+})

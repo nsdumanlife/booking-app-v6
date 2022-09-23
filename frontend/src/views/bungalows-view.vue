133b0facef56b7dcdebb7daab198faa07f7a7dcc
NSDuman <numanselmanduman@gmail.com>
2022-09-23 17:22:56 +0000

    Create bung-listing cp and split bung-view

--- frontend/src/views/bungalows-view.vue
+++ frontend/src/views/bungalows-view.vue
@@ -1,11 +1,10 @@
 <script>
-import BungalowCard from '@/components/bungalow-card.vue'
-import axios from 'axios'
+import BungalowsListing from '@/components/bungalows-listing.vue'
 import { mapActions } from 'vuex'
 
 export default {
   name: 'Bungalows',
-  components: { BungalowCard },
+  components: { BungalowsListing },
   data() {
     return {
       bungalows: [],
@@ -21,19 +20,8 @@ export default {
 </script>
 
 <template lang="pug">
-main.bungalow-list
-  h1 Rent a Bungalow for Your Next Escape
-
-  router-link(:to="`/bungalows/${bungalow._id}`" v-for="bungalow in bungalows") 
-    BungalowCard(:bungalow="bungalow")
-    
-
+.bungalow-list
+  BungalowsListing(:bungalows="bungalows")
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .bungalow-list {
-    min-height: 100vh;
-  }
-}
-</style>
+<style></style>

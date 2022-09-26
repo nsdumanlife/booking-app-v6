08201653a3b598ed25017de1d141ead5ffbcee4f
NSDuman <numanselmanduman@gmail.com>
2022-09-26 21:53:49 +0000

    Refactor code

--- backend/src/routes/account.js
+++ backend/src/routes/account.js
@@ -5,23 +5,26 @@ const User = require('../models/user')
 const router = express.Router()
 
 router.get('/session', (req, res) => {
-  res.send(req.session)
+  res.send(req.user)
 })
 
 router.post('/', async (req, res, next) => {
+  const { firstName, lastName, age, email, password } = req.body
+
   try {
-    const userToCreate = {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      age: req.body.age,
-      email: req.body.email,
-    }
+    // const userToCreate = {
+    //   firstName: req.body.firstName,
+    //   lastName: req.body.lastName,
+    //   age: req.body.age,
+    //   email: req.body.email,
+    //   password: req.body.password,
+    // }
 
-    const user = new User(userToCreate)
-    await user.setPassword(req.body.password)
-    await user.save()
+    const user = await User.register({ firstName, lastName, age, email }, password)
+    // await user.setPassword(req.body.password)
+    // await user.save()
 
-    return user
+    return res.send(user)
   } catch (e) {
     return next(e)
   }
@@ -31,9 +34,14 @@ router.post('/session', passport.authenticate('local', { failWithError: true }),
   res.send(req.user)
 })
 
-router.delete('/session', (req, res) => {
-  req.logOut()
-  res.send(true)
+router.delete('/session', async (req, res, next) => {
+  await req.logout()
+
+  req.session.regenerate(err => {
+    if (err) return next(err)
+
+    return res.sendStatus(200)
+  })
 })
 
 module.exports = router

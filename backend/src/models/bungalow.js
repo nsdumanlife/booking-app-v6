49b973b1844cf64f689cf6091daf8f789c01f890
NSDuman <numanselmanduman@gmail.com>
2022-09-27 11:42:55 +0000

    Fix image schema needed bug

--- backend/src/models/bungalow.js
+++ backend/src/models/bungalow.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const autopopulate = require('mongoose-autopopulate')
 const getDays = require('../helper/get-booking-days')
+const Image = require('./image')
 
 const bungalowSchema = new mongoose.Schema(
   {

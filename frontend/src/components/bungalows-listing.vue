01e19fdc35c9068d7a8c946e118acf59a5213cb5
NSDuman <numanselmanduman@gmail.com>
2022-10-03 19:19:13 +0000

    Remove slogan

--- frontend/src/components/bungalows-listing.vue
+++ frontend/src/components/bungalows-listing.vue
@@ -1,6 +1,6 @@
 <script>
-import BungalowCard from '@/components/bungalow-card.vue'
-import SearchBar from '@/components/search-bar.vue'
+import BungalowCard from '@/components/bungalow-card.vue';
+import SearchBar from '@/components/search-bar.vue';
 
 export default {
   name: 'BungalowsListing',
@@ -11,7 +11,6 @@ export default {
 
 <template lang="pug">
 main.bungalow-list
-  h1 Rent a Bungalow for Your Next Escape
   SearchBar
   router-link(:to="`/bungalows/${bungalow._id}`" v-for="bungalow in bungalows") 
     BungalowCard(:bungalow="bungalow")

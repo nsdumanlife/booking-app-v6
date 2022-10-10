f30f10971e46d11dbe57cc79dc32e3abfeca05cf
NSDuman <numanselmanduman@gmail.com>
2022-10-10 18:15:18 +0000

    Fix lost session bug

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,13 +1,29 @@
-<script setup>
+<script>
 // eslint-disable-next-line no-unused-vars
 import { RouterView } from 'vue-router'
+
 import HeaderCmp from './components/header.vue'
+import HeaderCmpCopy from './components/header-copy.vue'
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'App',
+  components: { HeaderCmp, HeaderCmpCopy },
+  methods: {
+    ...mapActions(['fetchSession']),
+  },
+  async created() {
+    await this.fetchSession()
+  },
+}
 </script>
 
 <template lang="pug">
-.header
-  HeaderCmp
-RouterView 
+#app
+  .header
+    HeaderCmp
+    //- HeaderCmpCopy
+  RouterView 
 </template>
 
 <style scoped></style>

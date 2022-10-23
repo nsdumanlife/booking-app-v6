cc9427cf5670ee19749076996255fcfa65faf129
NSDuman <numanselmanduman@gmail.com>
2022-10-23 19:58:06 +0000

    Create image gallery cmp

---
+++ frontend/src/components/image-gallery.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  name: 'ImageGallery',
+  props: ['bungalow'],
+}
+</script>
+
+<template lang="pug">
+.image-gallery
+  .img(v-for='image in bungalow.images')
+    router-link(:to='`/bungalows/${bungalow._id}/image/${image._id}`')
+      img(:src='image.src', :alt='image.alt')
+</template>
+
+<style lang="scss" scoped>
+.image-gallery {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+  grid-gap: 1.2rem;
+  max-width: 80rem;
+  margin: 5rem auto;
+  padding: 0 5rem;
+
+  .img {
+    flex-basis: 13rem;
+    img {
+      width: 100%;
+      height: 22vw;
+      object-fit: cover;
+      border-radius: 0.75rem;
+    }
+  }
+}
+</style>

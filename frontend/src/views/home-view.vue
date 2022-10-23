a3c1bb0227af83829b85c4f4980c1fa4d007d013
NSDuman <numanselmanduman@gmail.com>
2022-10-23 19:55:00 +0000

    Add style to home page

--- frontend/src/views/home-view.vue
+++ frontend/src/views/home-view.vue
@@ -1,27 +1,50 @@
 <script>
 import SearchBar from '@/components/search-bar.vue'
-import SearchDetail from '@/components/search-detail.vue'
-import { mapState } from 'vuex'
 
 export default {
   name: 'HomeView',
-  components: { SearchBar, SearchDetail },
-  computed: {
-    ...mapState(['isSearchBarVisible']),
-  },
+  components: { SearchBar },
 }
 </script>
 
 <template lang="pug">
-main
-  h1 Rent a Bungalow for Your Next Escape
-  SearchBar
-  SearchDetail(v-show='isSearchBarVisible')
+main.hero-image
+  h1.hero-text Rent a Bungalow for Your Next Escape
+  .search-bar
+    SearchBar
 </template>
 
-<style scoped>
-body {
+<style lang="scss" scoped>
+// height: 100vh;
+// display: flex;
+// flex-direction: column;
+// justify-content: space-around;
+
+.hero-image {
   display: flex;
-  align-content: space-between;
+  flex-direction: column;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('../assets/hero.jpg');
+  min-height: 50em;
+  height: 50%;
+  align-self: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  align-items: flex-start;
+  justify-content: center;
+}
+
+.hero-text {
+  text-align: center;
+  color: white;
+  align-self: center;
+  font-size: 60px;
+  font-weight: 600;
+  line-height: 70px;
+  margin-bottom: 4rem;
+}
+
+.search-bar {
+  padding: 1.5rem;
 }
 </style>

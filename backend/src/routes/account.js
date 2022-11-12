b7f0664ec3e01617ad5c58a90fb6e989000c815b
NSDuman <numanselmanduman@gmail.com>
2022-11-12 19:20:37 +0000

    Remove celebrate for create account

--- backend/src/routes/account.js
+++ backend/src/routes/account.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const passport = require('passport')
-const { celebrate, Joi, errors, Segments } = require('celebrate')
 
 const router = express.Router()
 
@@ -10,29 +9,17 @@ router.get('/session', (req, res) => {
   res.send(req.user)
 })
 
-router.post(
-  '/',
-  celebrate({
-    [Segments.BODY]: Joi.object().keys({
-      firstName: Joi.string().required(),
-      lastName: Joi.string().required(),
-      age: Joi.number().integer(),
-      email: Joi.string().email().required(),
-      // password: Joi.password().required(),
-    }),
-  }),
-  async (req, res, next) => {
-    const { firstName, lastName, age, email, password } = req.body
+router.post('/', async (req, res, next) => {
+  const { firstName, lastName, age, email, password } = req.body
 
-    try {
-      const user = await User.register({ firstName, lastName, age, email }, password)
+  try {
+    const user = await User.register({ firstName, lastName, age, email }, password)
 
-      return res.send(user)
-    } catch (e) {
-      return next(e)
-    }
+    return res.send(user)
+  } catch (e) {
+    return next(e)
   }
-)
+})
 
 router.post('/session', passport.authenticate('local', { failWithError: true }), async (req, res) => {
   res.send(req.user)
